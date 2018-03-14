124f51e3f78b1d71d71fe7d0b9df703d1e018a73
Maximiliano Barragan <maxx852@yahoo.com>
2018-03-14 07:19:28 -0700

    Adding triggers for pages turn after enough control movement

--- UnityVRTK/Assets/BookContent.cs
+++ UnityVRTK/Assets/BookContent.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BookContent : MonoBehaviour {
+public class BookContent : MonoBehaviour
+{
 
-	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RightPage")
+        {
+
+        }
+    }
 }
