b0bfa8393392ebe14d74f49ed3dbecdc1c107df5
Adam <funkylogic.studio@gmail.com>
2018-03-14 01:20:17 -0700

    iOS app added

---
+++ iOS/3DSCANER/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  3DSCANER
+//
+//  Created by Adam on 14/03/2018.
+//  Copyright © 2018 Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func LoadImageTap(_ sender: Any) {
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
