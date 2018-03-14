302ef497435115dfd52c9c871ce6f120f2d32d9b
Maximiliano Barragan <maxx852@yahoo.com>
2018-03-14 02:41:16 -0700

    Added book scene flip interaction.

---
+++ UnityVRTK/Assets/Page.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cover")
+        {
+            Debug.Log("Collision Detected");
+            rb.isKinematic = false;
+        }
+    }
+    
+
+    private IEnumerator PageEffect()
+    {
+        yield return new WaitForSeconds(1);
+
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+    }
+}
