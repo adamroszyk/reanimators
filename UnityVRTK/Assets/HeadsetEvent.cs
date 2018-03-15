320aedc62f8e8634957b97e97b7f3d2ab414c960
Maximiliano Barragan <maxx852@yahoo.com>
2018-03-14 17:33:43 -0700

    Making immersive scene with page 3 and Head.

---
+++ UnityVRTK/Assets/HeadsetEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadsetEvent : MonoBehaviour {
+    public Material matImmersion;
+    public List<GameObject> dissappearObjects;
+    public GameObject immersiveEnvrionment;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SomethingHappened()
+    {
+        RenderSettings.skybox = matImmersion;
+        immersiveEnvrionment.SetActive(true);
+        for(int i = 0; i < dissappearObjects.Count; i++)
+        {
+            dissappearObjects[i].SetActive(false);
+
+        }
+
+
+    }
+}
---
+++ UnityVRTK/Assets/Immerse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immerse : MonoBehaviour
+{
+    public Material immersiveMat;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ImmersiveExperience(Collider coli)
+    {
+        Debug.Log(coli);
+        Debug.Log("Switching Materials");
+        RenderSettings.skybox = immersiveMat;   
+    }
+}
